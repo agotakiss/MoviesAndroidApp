456cc4119b1d23940c5f1d5697f8c62f69c246fe
Agota Kiss <agota.kiss625@gmail.com>
2018-07-23 14:44:33 +0200

    after deleting every bullshit

--- app/src/main/java/com/agotakiss/androidtest/di/ActivityScope.kt
+++
@@ -1,8 +0,0 @@
-package com.agotakiss.androidtest.di
-
-import javax.inject.Scope
-
-@Scope
-@Retention(value = AnnotationRetention.RUNTIME)
-annotation class ActivityScope {
-}
--- app/src/main/java/com/agotakiss/androidtest/di/Injector.kt
+++ app/src/main/java/com/agotakiss/androidtest/di/Injector.kt
@@ -1,69 +1,60 @@
 package com.agotakiss.androidtest.di
 
-import android.app.Activity
+import com.agotakiss.androidtest.data.network.MovieDbApi
+import com.agotakiss.androidtest.data.network.RetrofitHelper
+import com.agotakiss.androidtest.data.repository.GenreRepositoryImpl
+import com.agotakiss.androidtest.data.repository.MovieRepositoryImpl
+import com.agotakiss.androidtest.data.store.GenreStore
+import com.agotakiss.androidtest.data.store.GenreStoreImpl
+import com.agotakiss.androidtest.domain.repository.GenreRepository
+import com.agotakiss.androidtest.domain.repository.MovieRepository
+import retrofit2.Retrofit
 
 
-class Injector private constructor() {
+object Injector {
 
-    companion object {
-        fun inject(activity: Activity) {
-            ActivityInjector.get(activity).inject(activity)
+    private var retrofit: Retrofit? = null
+
+    private var movieDbApi: MovieDbApi? = null
+
+    private var genreRepository: GenreRepository? = null
+
+    private var movieRepository: MovieRepository? = null
+
+    private var genreStore: GenreStore? = null
+
+    fun getRetrofit(): Retrofit {
+        if (retrofit == null) {
+            retrofit = RetrofitHelper.initRetrofit()
         }
-        fun clearComponent(activity: Activity){
-            ActivityInjector.get(activity).clear(activity)
-        }
-        fun inject(controller: Controller){
+        return retrofit!!
+    }
 
+    fun getMovieDbApi(): MovieDbApi {
+        if (movieDbApi == null) {
+            movieDbApi = getRetrofit().create(MovieDbApi::class.java)
         }
-        fun clearComponent(controller)
+        return movieDbApi!!
+    }
+
+    fun getGenreRepository(): GenreRepository {
+        if (genreRepository == null) {
+            genreRepository = GenreRepositoryImpl()
+        }
+        return genreRepository!!
+    }
+
+    fun getMovieRepository(): MovieRepository {
+        if (movieRepository == null) {
+            movieRepository = MovieRepositoryImpl()
+        }
+        return movieRepository!!
+    }
+
+    fun getGenreStore(): GenreStore {
+        if (genreStore == null) {
+            genreStore = GenreStoreImpl()
+        }
+        return genreStore!!
     }
 }
-
-
-//object Injector {
-//
-//    private var retrofit: Retrofit? = null
-//
-//    private var movieDbApi: MovieDbApi? = null
-//
-//    private var genreRepository: GenreRepository? = null
-//
-//    private var movieRepository: MovieRepository? = null
-//
-//    private var genreStore: GenreStore? = null
-//
-//    fun getRetrofit(): Retrofit {
-//        if (retrofit == null) {
-//            retrofit = RetrofitHelper.initRetrofit()
-//        }
-//        return retrofit!!
-//    }
-//
-//    fun getMovieDbApi(): MovieDbApi {
-//        if (movieDbApi == null) {
-//            movieDbApi = getRetrofit().create(MovieDbApi::class.java)
-//        }
-//        return movieDbApi!!
-//    }
-//
-//    fun getGenreRepository(): GenreRepository {
-//        if (genreRepository == null) {
-//            genreRepository = GenreRepositoryImpl()
-//        }
-//        return genreRepository!!
-//    }
-//
-//    fun getMovieRepository(): MovieRepository {
-//        if (movieRepository == null) {
-//            movieRepository = MovieRepositoryImpl()
-//        }
-//        return movieRepository!!
-//    }
-//
-//    fun getGenreStore(): GenreStore {
-//        if (genreStore == null) {
-//            genreStore = GenreStoreImpl()
-//        }
-//        return genreStore!!
-//    }
-//}
